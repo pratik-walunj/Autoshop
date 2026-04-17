93cef0b521982a8e3f550a01f99c67b58c5eabf2
Pratik Walunj <pratikwalunj487@gmail.com>
2026-04-17 18:04:42 +0530

    create  a insurance  page and nissan and renault model

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,16 +1,22 @@
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+
 import { Outlet } from "react-router-dom";
+import WhatsAppButton from "./pages/whatsappbutton";
 
 const Layout = () => {
   return (
     <>
       <Navbar />
       
+      
       <Outlet />   {/* 🔥 Dynamic page yaha render hoga */}
+      
 
       <Footer />
+      <WhatsAppButton/>
+      
     </>
   );
 };
